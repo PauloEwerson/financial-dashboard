b665f1af700b681f0965bece9792e15919aba990
PauloEwerson <ewersonmrt@gmail.com>
2024-10-23 21:43:20 -0300

    .

---
+++ src/components/Login/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LoginContainer = styled.div`
+
+`;
+
+const LoginForm: React.FC = () => {
+  return (
+    <LoginContainer>
+      <h1>Login</h1>
+    </LoginContainer>
+  );
+};
+
+export default LoginForm;
